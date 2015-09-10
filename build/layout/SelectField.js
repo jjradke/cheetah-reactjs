2921fb705a3af6491e96398ddf5f4c33e13df6a1
Dan <danwnuk@gmail.com>
2015-09-10 00:25:57 -0500

    #89

--- build/layout/SelectField.js
+++ build/layout/SelectField.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
 var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
@@ -14,15 +16,34 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactBootstrapMultiselect = require('react-bootstrap-multiselect');
+var _reactSelect = require('react-select');
 
-var _reactBootstrapMultiselect2 = _interopRequireDefault(_reactBootstrapMultiselect);
+var _reactSelect2 = _interopRequireDefault(_reactSelect);
+
+var _formsyReact = require('formsy-react');
+
+var _formsyReact2 = _interopRequireDefault(_formsyReact);
+
+var _reactMixin = require('react-mixin');
+
+var _reactMixin2 = _interopRequireDefault(_reactMixin);
 
 var SelectField = (function (_React$Component) {
     function SelectField(props) {
-        _classCallCheck(this, SelectField);
+        _classCallCheck(this, _SelectField);
 
-        _get(Object.getPrototypeOf(SelectField.prototype), 'constructor', this).call(this, props);
+        _get(Object.getPrototypeOf(_SelectField.prototype), 'constructor', this).call(this, props);
+
+        if (typeof props.value === 'string' && this.props.multiple) {
+            this.state = {
+                value: [props.value]
+            };
+        } else {
+            this.state = {
+                value: props.value
+            };
+        }
+
         this.state = { value: props.value };
 
         this.handleChange = this.handleChange.bind(this);
@@ -30,37 +51,22 @@ var SelectField = (function (_React$Component) {
 
     _inherits(SelectField, _React$Component);
 
-    _createClass(SelectField, [{
+    var _SelectField = SelectField;
+
+    _createClass(_SelectField, [{
         key: 'componentWillReceiveProps',
         value: function componentWillReceiveProps(nextProps) {
             this.forceUpdate();
         }
     }, {
-        key: 'getValue',
-        value: function getValue() {
-            return this.state.value;
-        }
-    }, {
         key: 'handleChange',
-        value: function handleChange(field, isSelected) {
-            var _this = this;
-
+        value: function handleChange(value, object) {
             if (this.props.multiple) {
-                if (isSelected) {
-                    this.state.value = this.props.options.filter(function (innerField) {
-                        return _this.getValueFromItem(innerField) == field.val() || _this.state.value.map(function (f) {
-                            return _this.getValueFromItem(f);
-                        }).indexOf(_this.getValueFromItem(innerField)) > -1;
-                    });
-                } else {
-                    this.state.value = this.state.value.filter(function (innerField) {
-                        return _this.getValueFromItem(innerField) != field.val();
-                    });
-                }
+                this.state.value = _.pluck(object, 'value');
             } else {
-                this.state.value = field.val();
+                this.state.value = value;
             }
-
+            this.setValue(this.state.value);
             this.props.onChange(this.state.value);
         }
     }, {
@@ -74,41 +80,44 @@ var SelectField = (function (_React$Component) {
             return this.props.valueKey ? item[this.props.valueKey] : item.Id;
         }
     }, {
-        key: 'componentDidMount',
+        key: 'render',
 
         /* Multiselect component we use does not support disabling - this will disable
          the button it renders. May want to switch to a new Multiselect. Will need to
          test in Ecommerce Web Admin if so aswell.
          */
-        value: function componentDidMount() {
-            $('.multiselect', _react2['default'].findDOMNode(this).parentNode).prop('disabled', this.props.disabled);
-        }
-    }, {
-        key: 'componentDidUpdate',
-        value: function componentDidUpdate() {
-            $('.multiselect', _react2['default'].findDOMNode(this).parentNode).prop('disabled', this.props.disabled);
-        }
-    }, {
-        key: 'render',
-        value: function render() {
-            var _this2 = this;
 
+        value: function render() {
             // get latest value from parent on each render
             this.state.value = this.props.value;
             var newValues = [];
             if (this.props.options) {
                 for (var i = 0; i < this.props.options.length; i++) {
-                    newValues.push({ value: this.getValueFromItem(this.props.options[i]), label: this.getLabel(this.props.options[i]),
-                        selected: !this.state.value ? false : this.props.multiple ? this.state.value.map(function (value) {
-                            return _this2.getValueFromItem(value);
-                        }).indexOf(this.getValueFromItem(this.props.options[i])) > -1 : this.state.value == this.getValueFromItem(this.props.options[i]) });
+                    newValues.push({
+                        value: this.getValueFromItem(this.props.options[i]),
+                        label: this.getLabel(this.props.options[i])
+                    });
                 }
             }
 
-            return _react2['default'].createElement(_reactBootstrapMultiselect2['default'], { nonSelectedText: 'Choose an option', data: newValues, onChange: this.handleChange, multiple: this.props.multiple, disabled: this.props.disabled, size: '2' });
+            var className = this.showRequired() ? 'required' : this.showError() ? 'error' : null;
+
+            var errorMessage = this.getErrorMessage();
+
+            return _react2['default'].createElement(
+                'span',
+                null,
+                _react2['default'].createElement(_reactSelect2['default'], _extends({}, this.props, { options: newValues, value: this.state.value, onChange: this.handleChange, multi: this.props.multiple, disabled: this.props.disabled, className: className })),
+                _react2['default'].createElement(
+                    'span',
+                    null,
+                    errorMessage
+                )
+            );
         }
     }]);
 
+    SelectField = _reactMixin2['default'].decorate(_formsyReact2['default'].Mixin)(SelectField) || SelectField;
     return SelectField;
 })(_react2['default'].Component);
 
